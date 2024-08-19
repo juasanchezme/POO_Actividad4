d58f64149dab6826c97716e24b5ce501184d1b10
Juanpbchb <131199500+Juanpbchb@users.noreply.github.com>
2024-08-19 13:28:10 -0500

    Add files via upload

---
+++ Actividad_4_Ejercicio_8.1/ListaPersonas.java
@@ -0,0 +1,25 @@
+
+package Personas;
+import java.util.*;
+/**
+ *
+ * @author Juan P
+ */
+public class ListaPersonas {
+    Vector listaPersonas;
+    
+    public ListaPersonas() {
+        listaPersonas = new Vector();
+    }
+    public void añadirPersona(Persona p) {
+        listaPersonas.add(p);
+    }
+    
+    public void eliminarPersona(int i) {
+        listaPersonas.removeElementAt(i);
+    }
+    
+    public void borrarLista() {
+        listaPersonas.removeAllElements();
+    }
+}
