df8d9984dadd127f7e3823923f2f734236fcc708
davidvv13 <161548966+davidvv13@users.noreply.github.com>
2024-08-20 17:14:33 -0500

    Add files via upload

---
+++ Ejer4.1/CuentaAhorros.java
@@ -0,0 +1,42 @@
+package ejer4.pkg1;
+
+public class CuentaAhorros extends Cuenta{
+    private boolean activa;
+    
+    public CuentaAhorros(float saldo, float tasa){
+        super(saldo, tasa);
+        if (saldo < 10000) 
+            activa = false;
+        else
+            activa = true;
+    }
+    
+    public void retirar(float cantidad){
+        if (activa)
+            super.retirar(cantidad);
+        
+    }
+    
+    public void consignar(float cantidad){
+        if (activa)
+            super.consignar(cantidad);
+    }
+    
+    public void extractoMensual(){
+        if (numeroRetiros > 4){
+            comisionMensual += (numeroRetiros - 4) * 1000;
+        }
+        super.extractoMensual();
+        if (saldo < 10000)
+            activa = false;
+    }
+    
+    public void imprimir(){
+        System.out.println("saldo = $" + saldo);
+        System.out.println("comisión mensual = $" + comisionMensual);
+        System.out.println("número de transacciones = " + (numeroConsignaciones
+                + numeroRetiros));
+        System.out.println();
+    }
+    
+}
